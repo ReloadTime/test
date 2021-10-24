efca882c0805f4249a950a79fe4c53d186302135
ReloadTime <93071248+ReloadTime@users.noreply.github.com>
2021-10-24 13:08:53 +0500

    This file is checkin your ping (google and yandex)

---
+++ WHAT.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo WHAT IS ABOUT UR PING???
+
+echo   -
+
+echo   -
+
+echo  GOOGLE:
+
+ping google.com /l 100
+
+echo   -
+
+echo   -
+
+echo ---------------------------------------------------------------
+
+echo YANDEX:
+
+ping yandex.ru /l 100
+
+timeout /t 120
+
+echo ===============================================================
+
+echo OKAY, LET'S START!
+
+timeout /t 100000000000
+
+start main.py
